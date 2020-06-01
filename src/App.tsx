b88ef25ad567d22072075e280aa572f7e96d229a
Ali <ali.ayman@optum.com>
2020-05-31 19:20:50 -0700

    fix gh-pages route parameter

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,10 @@ function App() {
       <Router>
         <OptumHeader />
         <Paper elevation={10} style={{ margin: '80px auto', height: '80vh', width: '90%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <Route path="/:id">
+          <Route exact path="/pages/aali66/optum-header/:id">
+            <Home />
+          </Route>
+          <Route exact path="/:id">
             <Home />
           </Route>
         </Paper>
